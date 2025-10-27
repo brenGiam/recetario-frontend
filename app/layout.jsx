b847b3a4381e7bf5c9ae05e15f15589bc83e41ab
Brenda Giambelluca <brengiambelluca@gmail.com>
2025-10-27 18:22:37 -0300

    PWA configured

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,13 @@
 import '@/app/styles/globals.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import PWARegister from './components/PWARegister';
 
 export const metadata = {
   title: "Mi recetario",
   description: "Recetario de uso personal",
-  authors: ['Brenda Giambelluca'],
+  authors: [{ name: 'Brenda Giambelluca' }],
+  manifest: '/manifest.json',
   icons: {
     icon: {
       url: "/favicon.ico",
@@ -14,15 +16,23 @@ export const metadata = {
   }
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  themeColor: '#e48f45',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="es-ar">
       <head>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
       <body>
+        <PWARegister />
         <Header />
         <main>{children}</main>
         <Footer />
